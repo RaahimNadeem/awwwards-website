9af15d0a64dbd6e7c43530a65d8f663deefb1bcf
R-Nade <114340940+RaahimNadeem@users.noreply.github.com>
2025-01-07 18:36:52 +0500

    Added story section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Story from "./components/Story";
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   );
 };
