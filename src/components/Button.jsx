077bcca6ea181b1df14c4c0b522f5f01e119c09e
R-Nade <114340940+RaahimNadeem@users.noreply.github.com>
2025-01-07 19:20:08 +0500

    Fix image reference

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
 
+// eslint-disable-next-line react/prop-types
 const Button = ({title, id, rightIcon, leftIcon, containerClass}) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
